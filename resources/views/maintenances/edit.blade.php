6f3ee1914ad1bd4efd5a33935292ed46a70df8f6
snipe <snipe@snipe.net>
2025-10-31 19:02:39 +0000

    Updated maintenances screen

--- resources/views/maintenances/edit.blade.php
+++ resources/views/maintenances/edit.blade.php
@@ -33,46 +33,37 @@
         @if ($item->id)
 
           @if ($item->asset)
-              <x-form-row>
-
+              <x-form-row name="asset">
                   <x-form-label>{{ trans('general.asset') }}</x-form-label>
-
                   <x-form-input>
                       <x-input.static>
                             {{ $item->asset->display_name }}
                       </x-input.static>
                   </x-form-input>
-
               </x-form-row>
 
               @if ($item->asset->company)
-                  <x-form-row>
-
-                          <x-form-label>{{ trans('general.company') }}</x-form-label>
-
-                          <x-form-input>
-                              <x-input.static>
-                                  {{ $item->asset->company->display_name }}
-                              </x-input.static>
-                          </x-form-input>
-
+                  <x-form-row name="company">
+                      <x-form-label>{{ trans('general.company') }}</x-form-label>
+                      <x-form-input>
+                          <x-input.static>
+                              {{ $item->asset->company->display_name }}
+                          </x-input.static>
+                      </x-form-input>
                   </x-form-row>
 
               @endif
 
               @if ($item->asset->location)
-                  <x-form-row>
-
+                  <x-form-row name="location">
                       <x-form-label>
                           {{ trans('general.location') }}
                       </x-form-label>
-
                       <x-form-input>
                           <x-input.static>
                               {{ $item->asset->location->display_name }}
                           </x-input.static>
                       </x-form-input>
-
                   </x-form-row>
               @endif
 
@@ -81,33 +72,28 @@
         @endif
 
           <!-- Name -->
-          <x-form-row>
+          <x-form-row name="name">
               <x-form-label>{{ trans('general.name') }}</x-form-label>
-
               <x-form-input>
-                <x-input.text
-                    name="name"
-                    :value="$item->name"
-                    required="true"
-                />
+                  <x-input.text
+                      required="true"
+                      :value="old('name', $item->name)"
+                  />
               </x-form-input>
-
           </x-form-row>
 
          @if (!$item->id)
-
           <!-- Assets (multiple select for new maintenance) -->
-          <x-form-row>
+          <x-form-row name="selected_assets[]">
                   <x-form-label>{{ trans('general.assets') }}</x-form-label>
                   <x-form-input>
                       <x-input.select2-ajax
                           :item="$item->id ? $item->asset()->pluck('id')->toArray() : old('selected_assets')"
                           item_model="\App\Models\Asset"
-                          name="selected_assets[]"
-                          required="true"
+                          :required="Helper::checkIfRequired($item, 'asset_id')"
                           multiple="true"
                           data_endpoint="hardware"
-                          :selected="$item->id ? $item->asset()->pluck('id')->toArray() : old('selected_assets')"
+                          :selected="old('selected_assets[]', request('asset_id'))"
                           :data_placeholder="trans('general.select_asset')"
                       />
                   </x-form-input>
@@ -115,68 +101,75 @@
          @endif
 
         <!-- Maintenance Type -->
-          <x-form-row>
+          <x-form-row name="asset_maintenance_type">
               <x-form-label>{{ trans('admin/asset_maintenances/form.asset_maintenance_type') }}</x-form-label>
               <x-form-input>
                   <x-input.select
-                      name="asset_maintenance_type"
                       :options="$maintenanceType"
                       :selected="old('asset_maintenance_type', $item->asset_maintenance_type)"
                       :required="Helper::checkIfRequired($item, 'asset_maintenance_type')"
                       data-placeholder="{{ trans('admin/maintenances/form.select_type')}}"
                       includeEmpty="true"
                       style="width:100%;"
-                      aria-label="asset_maintenance_type"
                   />
               </x-form-input>
           </x-form-row>
 
 
         <!--- Start Date -->
-          <x-form-row>
+          <x-form-row name="start_date">
                   <x-form-label>{{ trans('admin/maintenances/form.start_date') }}</x-form-label>
-
                   <x-form-input class="col-md-5">
-                      <x-input.datepicker
-                          name="start_date"
-                          :value="$item->start_date"
-                          required="true"
-                      />
+                      <x-input.datepicker :value="old('start_date', $item->start_date)" required="true" />
                   </x-form-input>
           </x-form-row>
 
 
         <!--- Completion Date -->
-          <x-form-row>
+          <x-form-row name="completion_date">
                   <x-form-label>{{ trans('admin/maintenances/form.completion_date') }}</x-form-label>
-
                   <x-form-input class="col-md-5">
-                      <x-input.datepicker
-                              name="completion_date"
-                              :value="$item->completion_date"
+                      <x-input.datepicker :value="old('completion_date', $item->completion_date)"
                       />
                   </x-form-input>
           </x-form-row>
 
-        <!-- URL -->
-          <x-form-row>
-                  <x-form-label>{{ trans('general.url') }}</x-form-label>
+          <!-- Cost -->
+          <x-form-row name="cost">
+              <x-form-label>{{ trans('admin/maintenances/form.cost') }}</x-form-label>
+              <x-form-input class="col-md-5">
+                  <x-input.text
+                          type="number"
+                          :input_group_text="$snipeSettings->default_currency"
+                          :value="old('cost', $item->cost)"
+                          input_group_addon="left"
+                          input_max="99999999999999999.000"
+                          input_min="0"
+                          input_min="0.00"
+                          input_step="0.001"
+                          maxlength="25"
+                  />
+              </x-form-input>
+          </x-form-row>
 
-                  <x-form-input>
-                      <x-input.text
+
+        <!-- URL -->
+          <x-form-row name="url">
+              <x-form-label>{{ trans('general.url') }}</x-form-label>
+              <x-form-input>
+                  <x-input.text
                           name="url"
                           type="url"
-                          :value="$item->url"
+                          :value="old('url', $item->url)"
                           input_icon="link"
                           input_group_addon="left"
                           placeholder="https://example.com"
-                      />
-                  </x-form-input>
+                  />
+              </x-form-input>
           </x-form-row>
 
-
         <!-- Supplier -->
-          <x-form-row>
+          <x-form-row name="supplier_id">
 
                   <x-form-label>{{ trans('general.supplier') }}</x-form-label>
 
@@ -185,7 +178,7 @@
                               item="$item->supplier"
                               item_model="\App\Models\Supplier"
                               name="supplier_id"
-                              :selected="old('supplier_id', ($item->supplier) ? $item->supplier->id : '')"
+                              :selected="old('supplier_id', $item->supplier_id)"
                               data_endpoint="suppliers"
                               :data_placeholder="trans('general.select_supplier')"
                       />
@@ -196,43 +189,26 @@
         <div class="form-group">
           <div class="col-sm-offset-3 col-sm-9">
               <label class="form-control">
-                <input type="checkbox" value="1" name="is_warranty" id="is_warranty" {{ old('is_warranty', $item->is_warranty) == '1' ? ' checked="checked"' : '' }}>
+                <input type="checkbox" value="1" name="is_warranty" id="is_warranty" @checked(old('is_warranty', $item->is_warranty))>
                 {{ trans('admin/maintenances/form.is_warranty') }}
               </label>
           </div>
         </div>
 
 
-        <!-- Cost -->
-          <x-form-row>
-              <x-form-label>{{ trans('admin/maintenances/form.cost') }}</x-form-label>
-
-              <x-form-input class="col-md-5">
-                  <x-input.text
-                          :input_group_text="$snipeSettings->default_currency"
-                          :value="$item->cost"
-                          input_group_addon="left"
-                          input_max="99999999999999999.000"
-                          input_min="0"
-                          input_min="0.00"
-                          input_step="0.001"
-                          maxlength="25"
-                          name="cost"
-                  />
-              </x-form-input>
-
-          </x-form-row>
-
         @include ('partials.forms.edit.image-upload', ['image_path' => app('maintenances_path')])
 
         <!-- Notes -->
-        <x-form-row
-                :label="trans('general.notes')"
-                :$item
-                name="notes"
-                type="textarea"
+        <x-form-row name="notes">
+            <x-form-label>{{ trans('general.notes') }}</x-form-label>
+            <x-form-input>
+              <x-input.textarea
+                :value="old('notes', $item->notes)"
                 placeholder="{{ trans('general.placeholders.notes') }}"
-        />
+              />
+            </x-form-input>
+        </x-form-row>
+
 
 
         <!-- End box component -->
